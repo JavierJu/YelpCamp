367045b48c695dd290988b82d9f2e9aee080db94
JavierJu <boswkd@gmail.com>
2025-01-23 10:53:39 +0900

    refactoring codes

--- app.js
+++ app.js
@@ -2,150 +2,67 @@ if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
 
-//Require
 const express = require('express');
-const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const flash = require('connect-flash');
-const methodOverride = require('method-override');
-const morgan = require('morgan');
-require('express-async-errors');
 const path = require('path');
+const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const mongoSanitize = require('express-mongo-sanitize');
-const helmet = require('helmet');
+require('express-async-errors');
+const ejsMate = require('ejs-mate');
 
+const connectDB = require('./config/db');
+const sessionConfig = require('./middlewares/session');
+const setupHelmet = require('./middlewares/helmet');
 const ExpressError = require('./utils/ExpressError');
+const User = require('./models/user');
+
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+
+// Routes
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const userRoutes = require('./routes/users');
-// const { campgroundSchema, reviewSchema } = require('./schemas');
-// const Campground = require('./models/campground');
-// const Review = require('./models/review');
-const User = require('./models/user');
+const homeRoutes = require('./routes/home');
 
 const app = express();
+connectDB();
 
-const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
-mongoose.connect(dbUrl)
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "Database connection error"));
-db.once("open", () => {
-    console.log("Database connected");
-});
-
-//EJS setting
+// EJS
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
-//Middleware
-app.use(morgan('tiny'));
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'));
+app.use(mongoSanitize());
+app.use(flash());
 app.use(express.static(path.join(__dirname, '/public')));
-app.use(
-    mongoSanitize({
-        replaceWith: '_',
-    }),
-);
+app.use(morgan('tiny'));
 
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
 const secret = process.env.SECRET || 'thisisnotagoodsecret';
 
-const store = MongoStore.create({
-    mongoUrl: dbUrl,
-    touchAfter: 24 * 60 * 60,
-    crypto: {
-        secret
-    }
-});
+// Session
+app.use(sessionConfig(dbUrl, secret));
 
-store.on("error", function (e) {
-    console.log("SESSION STORE ERROR", e)
-})
+//HelmsetupHt
+app.use(setupHelmet());
 
-const sessionConfig = {
-    store,
-    name: 'session', //기본은 connect.sid, 이름 변경으로 기본적인 웹 공격으로 부터 보호
-    secret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        HttpOnly: true,
-        secure: false, //배포 시 true
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}
-
-app.use(session(sessionConfig));
-app.use(flash());
-app.use(helmet());
-
-const scriptSrcUrls = [
-    "https://stackpath.bootstrapcdn.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://api.mapbox.com/",
-    "https://kit.fontawesome.com/",
-    "https://cdnjs.cloudflare.com/",
-    "https://cdn.jsdelivr.net",
-];
-const styleSrcUrls = [
-    "https://kit-free.fontawesome.com/",
-    "https://stackpath.bootstrapcdn.com/",
-    "https://api.mapbox.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://fonts.googleapis.com/",
-    "https://use.fontawesome.com/",
-    "https://cdn.jsdelivr.net", // 누락된 CDN 추가
-    "https://cdnjs.cloudflare.com/", // 누락된 CDN 추가
-];
-const connectSrcUrls = [
-    "https://api.mapbox.com/",
-    "https://a.tiles.mapbox.com/",
-    "https://b.tiles.mapbox.com/",
-    "https://events.mapbox.com/",
-];
-const fontSrcUrls = [
-    "https://cdnjs.cloudflare.com", // 추가된 CDN 도메인
-];
-app.use(
-    helmet.contentSecurityPolicy({
-        directives: {
-            defaultSrc: [],
-            connectSrc: ["'self'", ...connectSrcUrls],
-            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-            workerSrc: ["'self'", "blob:"],
-            objectSrc: [],
-            imgSrc: [
-                "'self'",
-                "blob:",
-                "data:",
-                "https://res.cloudinary.com/dwhuidaxq/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
-                "https://images.unsplash.com/",
-            ],
-            fontSrc: ["'self'", ...fontSrcUrls],
-        },
-    })
-);
-
+// Passport
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
-
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// Locals
 app.use((req, res, next) => {
-    console.log(req.query);
-
     // 특정 URL을 제외하고, 사용자가 요청한 URL을 세션에 저장
-    if (!['/login', '/'].includes(req.originalUrl)) {
+    if (!['/login', '/', '/register'].includes(req.originalUrl)) {
         req.session.returnTo = req.originalUrl;
     }
     // 현재 사용자 정보를 `res.locals`에 저장
@@ -156,17 +73,13 @@ app.use((req, res, next) => {
     next();
 })
 
-//External Routes
+// Routes
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/reviews', reviewRoutes);
 app.use('/', userRoutes);
+app.use('/', homeRoutes);
 
-//Home Route
-app.get('/', (req, res) => {
-    res.render('home')
-})
-
-//Error handlling
+// Error handlling
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
 });
@@ -177,7 +90,7 @@ app.use((err, req, res, next) => {
     res.status(status).render('error', { err });
 })
 
-// Open the server
+// Port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Serving on port ${port}`)
