0c5d89f8ebf6e27454aeeb84b443eb85d2af4b50
JavierJu <boswkd@gmail.com>
2025-02-19 15:12:37 +0900

    fix: updated users.login for saving session cookie first and redirection

--- controllers/users.js
+++ controllers/users.js
@@ -24,20 +24,35 @@ module.exports.renderLoginForm = (req, res) => {
     res.render('users/login');
 }
 
+// module.exports.login = async (req, res) => {
+//     req.flash('success', 'welcome back');
+
+//     // res.locals.returnTo에 저장된 URL 검증 및 수정
+//     let redirectUrl = res.locals.returnTo || '/campgrounds';
+
+//     // '/campgrounds/:id/reviews'인 경우 부모 페이지로 수정
+//     const match = redirectUrl.match(/^\/campgrounds\/([^/]+)\/reviews/);
+//     if (match) {
+//         redirectUrl = `/campgrounds/${match[1]}`;
+//     }
+
+//     res.redirect(redirectUrl); // 성공적으로 로그인 후 리다이렉트
+// }
+
 module.exports.login = async (req, res) => {
     req.flash('success', 'welcome back');
 
-    // res.locals.returnTo에 저장된 URL 검증 및 수정
     let redirectUrl = res.locals.returnTo || '/campgrounds';
 
-    // '/campgrounds/:id/reviews'인 경우 부모 페이지로 수정
     const match = redirectUrl.match(/^\/campgrounds\/([^/]+)\/reviews/);
     if (match) {
         redirectUrl = `/campgrounds/${match[1]}`;
     }
 
-    res.redirect(redirectUrl); // 성공적으로 로그인 후 리다이렉트
-}
+    req.session.save(() => {  // ✅ 세션을 저장한 후 리다이렉트 실행
+        res.redirect(redirectUrl);
+    });
+};
 
 module.exports.logout = (req, res) => {
     req.logout(function (err) {
